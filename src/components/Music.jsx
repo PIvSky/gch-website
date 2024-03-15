ec91d8f937fbb3d1cd8c604c8c3ead5af2b56f8d
PIvSky <piotr.iwansky@gmail.com>
2024-03-15 20:30:50 +0100

    iframe replaced by external library lite-youtube-embed in VideoTemplate component for better performance

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -5,15 +5,21 @@ const video = [
     {
         title: "Pokolenie - KOMBII (Grzeczni Chłopcy Cover)",
         id: Math.random(),
-        content: '',
-        src: 'https://www.youtube.com/embed/Si6HLWRcrKE?si=PIRpI_E1h8JfKlpZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        videoId: 'Si6HLWRcrKE',
+        src: 'https://www.youtube.com/embed/Si6HLWRcrKE?si=PIRpI_E1h8JfKlpZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share',
+        backgroundImageStyle: {
+            backgroundImage: "url('https://i.ytimg.com/vi/Si6HLWRcrKE/hqdefault.jpg')"
+        }
     },
     {
         title: "Grzeczni Chłopcy: Swingin' Boys- Za Tobą Pójdę Jak Na Bal (Krzysztof Krawczyk Cover)",
         id: Math.random(),
-        content: '',
-        src: 'https://www.youtube.com/embed/_d3rBDJxfrw?si=Hn-N3rZBGvMB_qaT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-    },
+        videoId: '_d3rBDJxfrw',
+        src: 'https://www.youtube.com/embed/_d3rBDJxfrw?si=Hn-N3rZBGvMB_qaT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share',
+        backgroundImageStyle: {
+            backgroundImage: "url('https://i.ytimg.com/vi/_d3rBDJxfrw/hqdefault.jpg')"
+        }
+    }
 ];
 
 const Music = () => {
@@ -23,8 +29,9 @@ const Music = () => {
                 <VideoTemplate
                 id={data.id}
                 title={data.title}
-                content={data.content}
+                videoId={data.videoId}
                 src={data.src}
+                backgroundImage={data.backgroundImageStyle}
             />
             ))}
         </div>
--- src/components/VideoTemplate.jsx
+++ src/components/VideoTemplate.jsx
@@ -1,20 +1,17 @@
 import style from '../styles/VideoTemplate.module.css';
 
-const VideoTemplate = ({title, content, src, id}) => {
+const VideoTemplate = ({title, id, videoId, backgroundImage}) => {
     return (
         <>
             <div className={`${style.video_wrapper}`}>
                 <h1 className={`${style.title}`}>{title}</h1>
-                <p className={`${style.content}`}>{content}</p>
                 <div className={`${style.container}`}>
-                        <iframe 
-                            loading="lazy"
-                            title={`Video of + ${title}`}
-                            key={id}
-                            width="300" 
-                            height="300" 
-                            src={src} allowfullscreen> 
-                        </iframe>
+                        <lite-youtube
+                            id={id} 
+                            videoid={videoId}
+                            params="controls=1"
+                            style={backgroundImage}
+                        />                        
                 </div>
             </div>
         </>
